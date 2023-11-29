b85eff6ee790e81c480b43cdcc818daea298cf59
Michael Carmelo <michaelcarmelo22@gmail.com>
2023-11-29 07:34:14 -0700

    First attempt to create readme

--- Develop/index.js
+++ Develop/index.js
@@ -54,10 +54,20 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeToFile('README.md', generateMarkdown(data));
+    });
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
